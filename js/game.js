025e669a72eaf8b123162b6688c36bf3953fcae7
VictorArguelho <varguelho69@gmail.com>
2026-04-08 16:31:36 -0300

    refactor: restructure game state and upgrade logic

--- js/game.js
+++ js/game.js
@@ -1,18 +1,72 @@
-export let gameState = {
+export const upgradeId = Object.freeze({
+  UPGRADE_1: "upgrade1",
+});
+
+export const gameState = {
   bolsonaroHelps: 0,
+
   helpsPerClick: 1,
-  upgradeCost: 10,
+  helpsPerSecond: 0,
 };
 
-export function addHelp() {
+export const upgrades = {
+  [upgradeId.UPGRADE_1]: {
+    text: "Aumentar ajuda",
+    level: 0,
+
+    startCost: 10,
+    multPerLevel: 1.1,
+
+    effects: {
+      clicksIncome: 1,
+      idleIncome: 0,
+    },
+  },
+};
+
+export function clicked() {
   gameState.bolsonaroHelps += gameState.helpsPerClick;
 }
 
-export function buyUpgrade() {
-  if (gameState.bolsonaroHelps >= gameState.upgradeCost) {
-    gameState.bolsonaroHelps -= gameState.upgradeCost;
-    gameState.helpsPerClick++;
-    return true;
+export function buyUpgrade(upgradeId) {
+  const upgrade = upgrades[upgradeId];
+
+  if (!upgrade) return;
+
+  if (hasEnoughHelps(getUpgradePrice(upgrade))) {
+    performUpgradeBuy(upgrade);
   }
-  return false;
+
+  recomputeStats();
+}
+
+function hasEnoughHelps(value) {
+  return gameState.bolsonaroHelps >= value;
+}
+
+function spendHelp(value) {
+  gameState.bolsonaroHelps -= value;
+}
+
+function performUpgradeBuy(upgrade) {
+  spendHelp(getUpgradePrice(upgrade));
+  upgrade.level++;
+}
+
+function getUpgradePrice(upgrade) {
+  const mult = upgrade.multPerLevel ** upgrade.level;
+  const price = upgrade.startCost * mult;
+  return Math.floor(price);
+}
+
+function recomputeStats() {
+  gameState.helpsPerClick = 1;
+  gameState.helpsPerSecond = 0;
+
+  for (const id in upgrades) {
+    const upg = upgrades[id];
+
+    gameState.helpsPerClick += upg.effects.clicksIncome * upg.level;
+    gameState.helpsPerSecond += upg.effects.idleIncome * upg.level;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-import { gameState, addHelp, buyUpgrade } from "./game.js";
+import { gameState, upgradeId, clicked, buyUpgrade } from "./game.js";
 import { elements, updateDisplay } from "./ui.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   setInterval(() => updateDisplay(gameState), 16);
 
-  elements.button.addEventListener("click", addHelp);
+  elements.button.addEventListener("click", clicked);
 
-  elements.upgradeBtn.addEventListener("click", buyUpgrade);
+  elements.upgradeBtn.addEventListener("click", () => {buyUpgrade(upgradeId.UPGRADE_1)});
 });
