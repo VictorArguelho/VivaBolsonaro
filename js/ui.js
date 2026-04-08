2c365fc7ed2f1c77812209a3d01e91bc9a3eb4df
VictorArguelho <varguelho69@gmail.com>
2026-04-08 18:43:09 -0300

    feat: added 'helps per second/click' display

--- js/ui.js
+++ js/ui.js
@@ -6,6 +6,8 @@ export const elements = {
   button: document.getElementById("bolsonaro-help-button"),
   text: document.getElementById("bolsonaro-helps-text"),
   upgradeBtn: document.getElementById("upgrade-bolsonaro-click-button"),
+  helpsPerClick: document.getElementById("helps-per-click"),
+  helpsPerSecond: document.getElementById("helps-per-second"),
 };
 
 const paths = {
@@ -17,4 +19,6 @@ export function updateDisplay() {
   elements.text.innerText = `Bolsonaro foi ajudado ${gameState.bolsonaroHelps} vezes`;
   elements.image.src = gameState.bolsonaroHelps === 0 ? paths.sad : paths.happy;
   elements.upgradeBtn.innerText = getUpgradeText(upgradeId.UPGRADE_1);
+  elements.helpsPerClick.innerText = `Ajudas por click: ${gameState.helpsPerClick}`;
+  elements.helpsPerSecond.innerText = `Ajudas por segundo: ${gameState.helpsPerSecond}`;
 }
