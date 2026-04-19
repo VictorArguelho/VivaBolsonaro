eed7783c84d7fd72cacc255dc7686f11cddb1f73
VictorArguelho <varguelho69@gmail.com>
2026-04-19 06:03:34 -0300

    fix: remove debug from loadSave

--- js/game/gameController.js
+++ js/game/gameController.js
@@ -48,9 +48,6 @@ export function getSave() {
 }
 
 export function loadSave(save) {
-  if(!save) console.log("objeto nulo");
-  if(!save.points) console.log("points nulo");
-  if(!save.upgrades) console.log("upgrades nulo");
   loadSavePoints(save?.points ?? null);
   loadSaveUpgrades(save?.upgrades ?? null);
 }
