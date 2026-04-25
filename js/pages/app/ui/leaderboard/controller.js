a84116f925037e9ca8078c8968fd0a2f882a160b
VictorArguelho <varguelho69@gmail.com>
2026-04-25 18:38:36 -0300

    feat: add cooldown to render leaderboard

--- js/pages/app/ui/leaderboard/controller.js
+++ js/pages/app/ui/leaderboard/controller.js
@@ -1,5 +1,12 @@
 import { render } from '@appUI/leaderboard/render.js';
+import { Timer } from '@utils/objects/timer.js';
+
+const renderTimer = new Timer(5000);
 
 export async function update() {
-  await render();
+  renderTimer.update();
+
+  if (renderTimer.isReady()) {
+    await render();
+  }
 }
